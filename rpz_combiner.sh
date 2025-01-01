9275a0496a2ad8d5efe3b4f9ec766ed561779a0f
ngrn <ngrn@noreply.localhost>
2025-01-01 12:40:31 +0000

    Update to 1.0.8

--- rpz_combiner.sh
+++ rpz_combiner.sh
@@ -2,7 +2,7 @@
 
 ################################################################################
 # rpz_blocklist_combiner.sh
-# Version: 1.0.7 Knot-resolver (for Ubuntu)
+# Version: 1.0.8 Knot-resolver (for Ubuntu/Debian)
 # An amateurish shell script to download, make consistent, and merge RPZ
 # blocklists for Knot Resolver, deleting duplicate entries. Now with user
 # allowlisting and user blocklisting, oo-ee.
@@ -12,19 +12,14 @@
 #set -x
 
 # Eat your variables
-readonly CHOWN="/usr/bin/chown"
 readonly CP="/usr/bin/cp"
 readonly CURL="/usr/bin/curl"
 readonly DATE="/usr/bin/date"
 readonly GREP="/usr/bin/grep"
-readonly INIT_SERVICE="/usr/bin/systemctl"
 readonly MV="/usr/bin/mv"
-readonly RM="/usr/bin/rm"
 readonly SED="/usr/bin/sed"
 readonly SORT="/usr/bin/sort"
 
-readonly RESOLVER_USER="knot-resolver"
-readonly RESOLVER_GROUP="knot-resolver"
 readonly RESOLVER_CONF_DIR="/etc/knot-resolver/blocklist_combiner"
 
 readonly LIST_URL_01="https://raw.githubusercontent.com/hagezi/dns-blocklists/main/rpz/ultimate.txt"
@@ -32,17 +27,11 @@ readonly LIST_URL_02="https://big.oisd.nl/rpz"
 readonly LIST_URL_03="https://raw.githubusercontent.com/badmojr/1Hosts/master/Pro/rpz.txt"
 readonly LIST_URL_04="https://urlhaus.abuse.ch/downloads/rpz/"
 
-readonly LIST_TMPFILE_01="$RESOLVER_CONF_DIR/blocklist_hagezi-multi-ultimate.tmp"
-readonly LIST_TMPFILE_02="$RESOLVER_CONF_DIR/blocklist_oisd-big.tmp"
-readonly LIST_TMPFILE_03="$RESOLVER_CONF_DIR/blocklist_1hosts-pro.tmp"
-readonly LIST_TMPFILE_04="$RESOLVER_CONF_DIR/blocklist_urlhaus.tmp"
-
-readonly LIST_TMPFILE_ALL="$RESOLVER_CONF_DIR/blocklist_*.tmp"
+readonly LIST_TMPFILE="$RESOLVER_CONF_DIR/blocklist_combined.tmp"
 
 readonly USER_ALLOWLIST="$RESOLVER_CONF_DIR/user_allowlist.txt"
 readonly USER_BLOCKLIST="$RESOLVER_CONF_DIR/user_blocklist.txt"
 
-readonly LIST_COMBINED_TMP="$RESOLVER_CONF_DIR/blocklist_combined.staging"
 readonly LIST_COMBINED="$RESOLVER_CONF_DIR/blocklist_combined.rpz"
 
 # RPZ header serial number = seconds since the UNIX Epoch (1970-01-01 00:00 UTC)
@@ -63,44 +52,39 @@ fi
 
 # Fetch the blocklists
 (>&2 printf "Downloading blocklists...\n")
-"$CURL" --silent "${LIST_URL_01}" -o "${LIST_TMPFILE_01}" || { (>&2 printf "%s failed to download, exiting\n" "${LIST_URL_01}") ; exit 1; }
-"$CURL" --silent "${LIST_URL_02}" -o "${LIST_TMPFILE_02}" || { (>&2 printf "%s failed to download, exiting\n" "${LIST_URL_02}") ; exit 1; }
-"$CURL" --silent "${LIST_URL_03}" -o "${LIST_TMPFILE_03}" || { (>&2 printf "%s failed to download, exiting\n" "${LIST_URL_03}") ; exit 1; }
-"$CURL" --silent "${LIST_URL_04}" -o "${LIST_TMPFILE_04}" || { (>&2 printf "%s failed to download, exiting\n" "${LIST_URL_04}") ; exit 1; }
+"$CURL" --silent "${LIST_URL_01}" -o "${LIST_TMPFILE}" || { (>&2 printf "%s failed to download, exiting\n" "${LIST_URL_01}") ; exit 1; }
+"$CURL" --silent "${LIST_URL_02}" >> "${LIST_TMPFILE}" || { (>&2 printf "%s failed to download, exiting\n" "${LIST_URL_02}") ; exit 1; }
+"$CURL" --silent "${LIST_URL_03}" >> "${LIST_TMPFILE}" || { (>&2 printf "%s failed to download, exiting\n" "${LIST_URL_03}") ; exit 1; }
+"$CURL" --silent "${LIST_URL_04}" >> "${LIST_TMPFILE}" || { (>&2 printf "%s failed to download, exiting\n" "${LIST_URL_04}") ; exit 1; }
 
 # Strip comments and any existing RPZ headers
-"$SED" -i '/^\;.*$/d' ${LIST_TMPFILE_ALL} || { (>&2 printf "Something went wrong processing %s, exiting\n" "${LIST_TMPFILE_ALL}") ; exit 1; }
-"$SED" -i '/^\@.*$/d' ${LIST_TMPFILE_ALL} || { (>&2 printf "Something went wrong processing %s, exiting\n" "${LIST_TMPFILE_ALL}") ; exit 1; }
-"$SED" -i '/^\$.*$/d' ${LIST_TMPFILE_ALL} || { (>&2 printf "Something went wrong processing %s, exiting\n" "${LIST_TMPFILE_ALL}") ; exit 1; }
-"$SED" -i '/^.*NS.*$/d' ${LIST_TMPFILE_ALL} || { (>&2 printf "Something went wrong processing %s, exiting\n" "${LIST_TMPFILE_ALL}") ; exit 1; }
+"$SED" -i '/^\;.*$/d' ${LIST_TMPFILE} || { (>&2 printf "Something went wrong processing %s, exiting\n" "${LIST_TMPFILE_ALL}") ; exit 1; }
+"$SED" -i '/^\@.*$/d' ${LIST_TMPFILE} || { (>&2 printf "Something went wrong processing %s, exiting\n" "${LIST_TMPFILE_ALL}") ; exit 1; }
+"$SED" -i '/^\$.*$/d' ${LIST_TMPFILE} || { (>&2 printf "Something went wrong processing %s, exiting\n" "${LIST_TMPFILE_ALL}") ; exit 1; }
+"$SED" -i '/^.*NS.*$/d' ${LIST_TMPFILE} || { (>&2 printf "Something went wrong processing %s, exiting\n" "${LIST_TMPFILE_ALL}") ; exit 1; }
 # Strips inline comments from urlhaus list
-"$SED" -i 's/\ \;\ .*//' ${LIST_TMPFILE_ALL} || { (>&2 printf "Something went wrong processing %s, exiting\n" "${LIST_TMPFILE_ALL}") ; exit 1; }
+"$SED" -i 's/\ \;\ .*//' ${LIST_TMPFILE} || { (>&2 printf "Something went wrong processing %s, exiting\n" "${LIST_TMPFILE_ALL}") ; exit 1; }
+"$SED" -i 's/\.\;\ .*/\./' ${LIST_TMPFILE} || { (>&2 printf "Something went wrong processing %s, exiting\n" "${LIST_TMPFILE_ALL}") ; exit 1; }
 
 # Add the wildcard domains to the urlhaus list
-if test -f "${LIST_TMPFILE_04}"; then
-    "$GREP" -E -v '^#|^$' ${LIST_TMPFILE_04} | while IFS= read -r URLHAUS_BLOCKLIST_ENTRY; do
-        printf "*.%s\n" "${URLHAUS_BLOCKLIST_ENTRY}" >> ${LIST_TMPFILE_04} || { (>&2 printf "Something went wrong processing %s, exiting\n" "${LIST_TMPFILE_04}") ; exit 1; }
-    done
-fi
+#if test -f "${LIST_TMPFILE_04}"; then
+#    "$GREP" -E -v '^#|^$' ${LIST_TMPFILE_04} | while IFS= read -r URLHAUS_BLOCKLIST_ENTRY; do
+#        printf "*.%s\n" "${URLHAUS_BLOCKLIST_ENTRY}" >> ${LIST_TMPFILE_04} || { (>&2 printf "Something went wrong processing %s, exiting\n" "${LIST_TMPFILE_04}") ; exit 1; }
+#    done
+#fi
 
 # Delete lines longer than 256 character to avoid a bug in Unbound (248 characters + the later added ' CNAME .')
-"$SED" -i '/^.\{247\}./d' ${LIST_TMPFILE_ALL} || { (>&2 printf "Something went wrong checking line lengths, exiting\n") ; exit 1; }
-"$SED" -i '/^\*\..\{247\}./d' ${LIST_TMPFILE_ALL} || { (>&2 printf "Something went wrong checking line lengths, exiting\n") ; exit 1; }
+"$SED" -i '/^.\{247\}./d' ${LIST_TMPFILE} || { (>&2 printf "Something went wrong checking line lengths, exiting\n") ; exit 1; }
+"$SED" -i '/^\*\..\{247\}./d' ${LIST_TMPFILE} || { (>&2 printf "Something went wrong checking line lengths, exiting\n") ; exit 1; }
 
 (>&2 printf "Creating the combined blocklist...\n")
-
-# Combine the .tmp files and append them to the combined blocklist file
-"$SORT" -u -f ${LIST_TMPFILE_ALL} > "${LIST_COMBINED_TMP}" || { (>&2 printf "Something went wrong combining the blocklists, exiting\n") ; exit 1; }
-
-# Add the RPZ headers at the start of the file
-"$SED" -i "1s/^/$RPZ_HEADER_LINE_01$RPZ_HEADER_LINE_02$RPZ_HEADER_LINE_03/" "${LIST_COMBINED_TMP}" || { (>&2 printf "Something went wrong adding the RPZ header, exiting\n") ; exit 1; }
 
 # Basic blocklist filtering
 if test -f "${USER_BLOCKLIST}"; then
     (>&2 printf "Processing user blocklist...\n")
     "$GREP" -E -v '^#|^$' ${USER_BLOCKLIST} | while IFS= read -r USER_BLOCKLIST_ENTRY; do
-        printf "*.%s CNAME .\n" "${USER_BLOCKLIST_ENTRY}" >> ${LIST_COMBINED_TMP} || { (>&2 printf "Something went wrong processing %s, exiting\n" "${USER_BLOCKLIST}") ; exit 1; }
-        printf "%s CNAME .\n" "${USER_BLOCKLIST_ENTRY}" >> ${LIST_COMBINED_TMP} || { (>&2 printf "Something went wrong processing %s, exiting\n" "${USER_BLOCKLIST}") ; exit 1; }
+        printf "*.%s CNAME .\n" "${USER_BLOCKLIST_ENTRY}" >> ${LIST_TMPFILE} || { (>&2 printf "Something went wrong processing %s, exiting\n" "${USER_BLOCKLIST}") ; exit 1; }
+        printf "%s CNAME .\n" "${USER_BLOCKLIST_ENTRY}" >> ${LIST_TMPFILE} || { (>&2 printf "Something went wrong processing %s, exiting\n" "${USER_BLOCKLIST}") ; exit 1; }
     done
 fi
 
@@ -108,15 +92,16 @@ fi
 if test -f "${USER_ALLOWLIST}"; then
     (>&2 printf "Processing user allowlist...\n")
     "$GREP" -E -v '^#|^$' ${USER_ALLOWLIST} | while IFS= read -r USER_ALLOWLIST_ENTRY; do
-        "$SED" -i "/\*\."${USER_ALLOWLIST_ENTRY}"\ CNAME\ \./d" ${LIST_COMBINED_TMP} || { (>&2 printf "Something went wrong processing %s, exiting\n" "${USER_ALLOWLIST}") ; exit 1; }
-        "$SED" -i "/"${USER_ALLOWLIST_ENTRY}"\ CNAME\ \./d" ${LIST_COMBINED_TMP} || { (>&2 printf "Something went wrong processing %s, exiting\n" "${USER_ALLOWLIST}") ; exit 1; }
+        "$SED" -i "/\*\."${USER_ALLOWLIST_ENTRY}"\ CNAME\ \./d" ${LIST_TMPFILE} || { (>&2 printf "Something went wrong processing %s, exiting\n" "${USER_ALLOWLIST}") ; exit 1; }
+        "$SED" -i "/"${USER_ALLOWLIST_ENTRY}"\ CNAME\ \./d" ${LIST_TMPFILE} || { (>&2 printf "Something went wrong processing %s, exiting\n" "${USER_ALLOWLIST}") ; exit 1; }
     done
 fi
 
-"$MV" "${LIST_COMBINED_TMP}" "${LIST_COMBINED}" || { (>&2 printf "Moving the blocklist .tmp file to its final destination failed, exiting\n") ; exit 1; }
+# Sort the list into alphabetical order
+"$SORT" -o ${LIST_TMPFILE} -u -f ${LIST_TMPFILE} || { (>&2 printf "Something went wrong sorting the blocklists, exiting\n") ; exit 1; }
 
-"$RM" ${LIST_TMPFILE_ALL}
+# Add the RPZ headers at the start of the file
+"$SED" -i "1s/^/$RPZ_HEADER_LINE_01$RPZ_HEADER_LINE_02$RPZ_HEADER_LINE_03/" "${LIST_TMPFILE}" || { (>&2 printf "Something went wrong adding the RPZ header, exiting\n") ; exit 1; }
 
-# Make sure everything is owned by RESOLVER_USER and RESOLVER_GROUP
-#(>&2 printf "Changing user and group of Knot Resolver configuration directory...\n")
-#"$CHOWN" -R ${RESOLVER_USER}:${RESOLVER_GROUP} ${RESOLVER_CONF_DIR} || { (>&2 printf "Something went wrong while changing ownership of the %s directory, exiting\n" "${RESOLVER_CONF_DIR}") ; exit 1; }
+# Move the .tmp file into the correct place
+"$MV" "${LIST_TMPFILE}" "${LIST_COMBINED}" || { (>&2 printf "Moving the blocklist .tmp file to its final destination failed, exiting\n") ; exit 1; }
